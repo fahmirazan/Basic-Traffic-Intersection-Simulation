d596a2f07fe902324c476f3b3bd89225a07082fb
fahmirazan <61136014+fahmirazan@users.noreply.github.com>
2022-06-20 22:52:03 +0700

    update

--- simulation.py
+++ simulation.py
@@ -4,9 +4,9 @@ import threading
 import pygame
 import sys
 
-speeds = {'car': random.randint(1.0, 5.0)}  # average speeds of vehicles
+speeds = {'car': random.randint(1.0, 5.0)}
 
-# Coordinates of vehicles' start
+
 x = {'right':[0,0,0], 'left':[1400,1400,1400]}    
 y = {'right':[348,370,398], 'left':[498,466,436]}
 
@@ -14,9 +14,8 @@ vehicles = {'right': {0:[], 1:[], 2:[], 'crossed':0}, 'left': {0:[], 1:[], 2:[],
 vehicleTypes = {0:'car'}
 directionNumbers = {0:'right'}
 
-# Gap between vehicles
-stoppingGap = 1   # stopping gap
-movingGap = 1   # moving gap
+stoppingGap = 1 
+movingGap = 1 
 
 pygame.init()
 simulation = pygame.sprite.Group()
@@ -37,7 +36,6 @@ class Vehicle(pygame.sprite.Sprite):
         path = "images/" + direction + "/" + vehicleClass + ".png"
         self.image = pygame.image.load(path)
             
-        # Set new starting and stopping coordinate
         if(direction=='right'):
             temp = self.image.get_rect().width + stoppingGap    
             x[direction][lane] -= temp
@@ -49,13 +47,12 @@ class Vehicle(pygame.sprite.Sprite):
     def move(self):
         a=random.randint(1,10)
         if(self.direction=='right'):
-            if(self.crossed==0):   # if the image has crossed stop line now
+            if(self.crossed==0):
                 self.crossed = 1
             if((self.index==0 or self.x+self.image.get_rect().width<(vehicles[self.direction][self.lane][self.index-1].x - movingGap))):                
-            # (if the image has not reached its stop coordinate or has crossed stop line or has green signal) and (it is either the first vehicle in that lane or it is has enough gap to the next vehicle in that lane)
-                self.x += self.speed +  a # move the vehicle
+                self.x += self.speed +  a
 
-# Generating vehicles in the simulation
+
 def generateVehicles():
     while(True):
         lane_number = (1)
@@ -68,27 +65,27 @@ def generateVehicles():
         time.sleep(0.2)
 
 class Main:
-    thread1 = threading.Thread(name="initialization", args=())    # initialization
+    thread1 = threading.Thread(name="initialization", args=())
     thread1.daemon = True
     thread1.start()
 
-    # Colours 
+
     black = (0, 0, 0)
     white = (255, 255, 255)
 
-    # Screensize 
+ 
     screenWidth = 1400
     screenHeight = 800
     screenSize = (screenWidth, screenHeight)
 
-    # Setting background image i.e. image of intersection
+
     background = pygame.image.load('images/intersection.png')
 
     screen = pygame.display.set_mode(screenSize)
     pygame.display.set_caption("SIMULATION")
     font = pygame.font.Font(None, 30)
 
-    thread2 = threading.Thread(name="generateVehicles",target=generateVehicles, args=())    # Generating vehicles
+    thread2 = threading.Thread(name="generateVehicles",target=generateVehicles, args=())
     thread2.daemon = True
     thread2.start()
 
@@ -97,9 +94,8 @@ class Main:
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.blit(background,(0,0))   # display background in simulation
+        screen.blit(background,(0,0))
 
-        # display the vehicles
         for vehicle in simulation:  
             screen.blit(vehicle.image, [vehicle.x, vehicle.y])
             vehicle.move()
